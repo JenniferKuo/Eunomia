33ddc628f9750fac210adab22dbc838832e1e792
TingYuKuo <meappp118@gmail.com>
2019-11-23 11:00:00 +0800

    fix selection

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+var currentText = "";
+var currentStartIndex = 0;
+var currentEndIndex = 1;
+var words;
+var myFocusOffset =  window.getSelection().focusOffset;
+var currentFunction;
+
 $(function() {
     // 偵測 Ctrl+S 儲存檔案
     $(window).bind('keydown', function(event) {
@@ -10,15 +17,15 @@ $(function() {
                     break;
                 // function 1
                 case 'y':
-                    f1();
+                    words = f1();
                     break;
                 // function 2
                 case 'u':
-                    f2();
+                    words = f2();
                     break;
                 // function 3
                 case 'i':
-                    f3();
+                    words = f3();
                     break;
             }
         }
@@ -51,14 +58,19 @@ function getTextPosition() {
     }
 }
 
-// TODO: 新增一個目前字詞的變數
 function autocomplete(inp, arr) {
     // arr 是建議字詞列表
     var currentFocus;
     // 建立一個包含所有建議字詞的list
     inp.bind("DOMSubtreeModified", function (e) {
-    /*execute a function when someone writes in the text field:*/
-        var a, b, i, val = inp.text();
+        // 移動斷詞的結尾到最後
+        // TODO: 現在如果刪除字的話會錯
+        currentEndIndex = inp.text().length;
+        currentText =  inp.text().slice(currentStartIndex, currentEndIndex);
+        arr = words;
+        // TODO: val應該是從上次斷掉的地方開始
+        var a, b, i, val = currentText;
+        // var a, b, i, val = currentText;
         console.log(val);
         /*close any already open lists of autocompleted values*/
         closeAllLists();
@@ -85,14 +97,10 @@ function autocomplete(inp, arr) {
             b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
             /*execute a function when someone clicks on the item value (DIV element):*/
             b.addEventListener("click", function(e) {
-                // TODO: 改成插入剩餘的字
-                // insertTextAtCursor(this.getElementsByTagName("input")[0].value);
-                // inp.text(this.getElementsByTagName("input")[0].value);
-                inp.html(this.getElementsByTagName("input")[0].value);
-                // $('#textarea').value = this.getElementsByTagName("input")[0].value;
-                /*close the list of autocompleted values,
-                (or any other open lists of autocompleted values:*/
+                insertTextAtCursor(this.getElementsByTagName("input")[0].value.substr(val.length));
                 closeAllLists();
+                // 把斷詞起始位置移到最後
+                currentStartIndex = inp.text().length;
             });
             a.appendChild(b);
           }
@@ -169,164 +177,33 @@ function autocomplete(inp, arr) {
 }
 
 function f1(){
-    // 選擇游標的前一個字
-    var sel, range;
-        if (window.getSelection) {
-            sel = window.getSelection();
-            if (sel.getRangeAt && sel.rangeCount) {
-                range = sel.getRangeAt(0);
-                selected = range.startContainer;
-                console.log(selected);
-                // range.deleteContents();
-                // range.insertNode( document.createTextNode(text) );
-            }
-        } else if (document.selection && document.selection.createRange) {
-            console.log("else" + document.selection.createRange);
-            // document.selection.createRange().text = text;
-        }
+    return words;
 }
 
 function f2(){
-    // 選擇游標前完整的一段話
-    var sel, range;
-    if (window.getSelection) {
-        sel = window.getSelection();
-        if (sel.getRangeAt && sel.rangeCount) {
-            range = sel.getRangeAt(0);
-            console.log(range.startContainer);
-            // range.deleteContents();
-            // range.insertNode( document.createTextNode(text) );
-        }
-    } else if (document.selection && document.selection.createRange) {
-        console.log("else" + document.selection.createRange);
-        // document.selection.createRange().text = text;
-    }
+    return words;
 }
 
 function f3(){
-    // 選擇游標前完整的一段話
-    var sel, range;
-    if (window.getSelection) {
-        sel = window.getSelection();
-        if (sel.getRangeAt && sel.rangeCount) {
-            range = sel.getRangeAt(0);
-            console.log(range.startContainer);
-            // range.deleteContents();
-            // range.insertNode( document.createTextNode(text) );
-        }
-    } else if (document.selection && document.selection.createRange) {
-        console.log("else" + document.selection.createRange);
-        // document.selection.createRange().text = text;
-    }
+    return words;
 }
-// function autocomplete2(inp, arr) {
-//     /*the autocomplete function takes two arguments,
-//     the text field element and an array of possible autocompleted values:*/
-//     var currentFocus;
-//     /*execute a function when someone writes in the text field:*/
-//     inp.addEventListener("input", function(e) {
-//         var a, b, i, val = this.value;
-//         /*close any already open lists of autocompleted values*/
-//         closeAllLists();
-//         if (!val) { return false;}
-//         currentFocus = -1;
-//         /*create a DIV element that will contain the items (values):*/
-//         a = document.createElement("DIV");
-//         a.setAttribute("id", this.id + "autocomplete-list");
-//         a.setAttribute("class", "autocomplete-items");
-//         /*append the DIV element as a child of the autocomplete container:*/
-//         this.parentNode.appendChild(a);
-//         /*for each item in the array...*/
-//         for (i = 0; i < arr.length; i++) {
-//           /*check if the item starts with the same letters as the text field value:*/
-//           if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-//             /*create a DIV element for each matching element:*/
-//             b = document.createElement("DIV");
-//             /*make the matching letters bold:*/
-//             b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-//             b.innerHTML += arr[i].substr(val.length);
-//             /*insert a input field that will hold the current array item's value:*/
-//             b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-//             /*execute a function when someone clicks on the item value (DIV element):*/
-//                 b.addEventListener("click", function(e) {
-//                 /*insert the value for the autocomplete text field:*/
-//                 inp.value = this.getElementsByTagName("input")[0].value;
-//                 console.log(this.getElementsByTagName("input")[0].value);
-//                 $('.ql-editor').html(this.getElementsByTagName("input")[0].value);
-//                 /*close the list of autocompleted values,
-//                 (or any other open lists of autocompleted values:*/
-//                 closeAllLists();
-//             });
-//             a.appendChild(b);
-//           }
-//         }
-//     });
-//     /*execute a function presses a key on the keyboard:*/
-//     inp.addEventListener("keydown", function(e) {
-//         var x = document.getElementById(this.id + "autocomplete-list");
-//         if (x) x = x.getElementsByTagName("div");
-//         if (e.keyCode == 40) {
-//           /*If the arrow DOWN key is pressed,
-//           increase the currentFocus variable:*/
-//           currentFocus++;
-//           /*and and make the current item more visible:*/
-//           addActive(x);
-//         } else if (e.keyCode == 38) { //up
-//           /*If the arrow UP key is pressed,
-//           decrease the currentFocus variable:*/
-//           currentFocus--;
-//           /*and and make the current item more visible:*/
-//           addActive(x);
-//         } else if (e.keyCode == 13) {
-//           /*If the ENTER key is pressed, prevent the form from being submitted,*/
-//           e.preventDefault();
-//           if (currentFocus > -1) {
-//             /*and simulate a click on the "active" item:*/
-//             if (x) x[currentFocus].click();
-//           }
-//         }
-//     });
-//     function addActive(x) {
-//       /*a function to classify an item as "active":*/
-//       if (!x) return false;
-//       /*start by removing the "active" class on all items:*/
-//       removeActive(x);
-//       if (currentFocus >= x.length) currentFocus = 0;
-//       if (currentFocus < 0) currentFocus = (x.length - 1);
-//       /*add class "autocomplete-active":*/
-//       x[currentFocus].classList.add("autocomplete-active");
-//     }
-//     function removeActive(x) {
-//       /*a function to remove the "active" class from all autocomplete items:*/
-//       for (var i = 0; i < x.length; i++) {
-//         x[i].classList.remove("autocomplete-active");
-//       }
-//     }
-//     function closeAllLists(elmnt) {
-//       /*close all autocomplete lists in the document,
-//       except the one passed as an argument:*/
-//       var x = document.getElementsByClassName("autocomplete-items");
-//       for (var i = 0; i < x.length; i++) {
-//         if (elmnt != x[i] && elmnt != inp) {
-//         x[i].parentNode.removeChild(x[i]);
-//       }
-//     }
-//   }
-//   /*execute a function when someone clicks in the document:*/
-//   document.addEventListener("click", function (e) {
-//       closeAllLists(e.target);
-//   });
-//   }
 
     // 在游標之後插入文字
     function insertTextAtCursor(text) {
         var sel, range;
         if (window.getSelection) {
             sel = window.getSelection();
+            window.getSelection().focusNode.nodeValue = text;
+            window.getSelection().focusNode.innerHTML = text;
+            console.log(window.getSelection().focusNode.nodeValue);
+            // window.getSelection().focusNode.insertNode( document.createTextNode(text));
             if (sel.getRangeAt && sel.rangeCount) {
                 range = sel.getRangeAt(0);
                 range.deleteContents();
-                range.insertNode( document.createTextNode(text) );
+                var node = document.createTextNode(text);
+                range.insertNode(node);
+                // TODO: 找到目前游標位置插入文字
+                document.getElementsByClassName("ql-editor")[0].lastChild.innerHTML += text;
             }
         } else if (document.selection && document.selection.createRange) {
             document.selection.createRange().text = text;
@@ -374,8 +251,7 @@ $(document).ready(function() {
     // 載入儲存的文字
     changeCSS();
     loaddata();
-    var words = ["勞動基準法第5條</br>雇主不得以強暴、脅迫、拘禁或其他非法之方法，強制勞工從事勞動。","勞動基準法第6條</br>任何人不得介入他人之勞動契約，抽取不法利益。","定期契約屆滿後，有左列情形之一者，視為不定期契約：<br />(一)勞工繼續工作而雇主不即表示反對意思者。<br />(二)雖經另訂新約，惟其前後勞動契約之工作期間超過九十日，前後契約間斷期間未超過三十日者。勞動基準法第9條第2項第1款","勞動契約，分為定期契約及不定期契約。臨時性、短期性、季節性及特定性工作得為定期契約；有繼續性工作應為不定期契約。派遣事業單位與派遣勞工訂定之勞動契約，應為不定期契約。<br />定期契約屆滿後，有左列情形之一者，視為不定期契約：<br />(一)勞工繼續工作而雇主不即表示反對意思者。<br />(二)雖經另訂新約，惟其前後勞動契約之工作期間超過九十日，前後契約間斷期間未超過三十日者。前項規定於特定性或季節性之定期工作不適用之。</br>勞動基準法第9條第1項","可允許使用之武力(正當防衛)", "可不可", "債主", "債務","債權受益人", "遺產管理人", "銀行帳戶", "前約因", "不成比例", "受拘束"];
+    words = ["勞動基準法第5條</br>雇主不得以強暴、脅迫、拘禁或其他非法之方法，強制勞工從事勞動。","勞動基準法第6條</br>任何人不得介入他人之勞動契約，抽取不法利益。","定期契約屆滿後，有左列情形之一者，視為不定期契約：<br />(一)勞工繼續工作而雇主不即表示反對意思者。<br />(二)雖經另訂新約，惟其前後勞動契約之工作期間超過九十日，前後契約間斷期間未超過三十日者。勞動基準法第9條第2項第1款","勞動契約，分為定期契約及不定期契約。臨時性、短期性、季節性及特定性工作得為定期契約；有繼續性工作應為不定期契約。派遣事業單位與派遣勞工訂定之勞動契約，應為不定期契約。<br />定期契約屆滿後，有左列情形之一者，視為不定期契約：<br />(一)勞工繼續工作而雇主不即表示反對意思者。<br />(二)雖經另訂新約，惟其前後勞動契約之工作期間超過九十日，前後契約間斷期間未超過三十日者。前項規定於特定性或季節性之定期工作不適用之。</br>勞動基準法第9條第1項","可允許使用之武力(正當防衛)", "可不可", "債主", "債務","債權受益人", "遺產管理人", "銀行帳戶", "前約因", "不成比例", "受拘束"];
     autocomplete($('.ql-editor'), words);
-    // autocomplete2(document.getElementById("textarea"), words);
 });
 
